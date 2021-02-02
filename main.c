2d78d3c7560d7d2d1484f3753d9e2750fe4fb1da
smm3123 <syedmahdi3123@gmail.com>
2021-02-02 09:53:55 -0600

    Add argument parsing

--- main.c
+++ main.c
@@ -7,9 +7,10 @@
 #define MAX_ARGS 512
 
 // Prototypes
-void getArguments(char* args);
 enum programState;
+void getArguments(char* args);
 enum programState executeInput(char* args);
+char** parseArguments(char* args);
 
 enum programState {
 	Okay,
@@ -26,6 +27,7 @@ int main() {
 		// Get user input
 		char args[MAX_ARG_LENGTH];
 		getArguments(args);
+		char** argsArray = parseArguments(args);
 
 		state = executeInput(args);
 	}
@@ -43,8 +45,24 @@ enum programState executeInput(char* args) {
 	// If the first character in the arg is '#', it's considered a comment - midline comments not within scope of project
 	if ((args == NULL || strcmp(args, "") == 0) || args[0] == '#')
 		return Okay;
-	if (strcmp(args, "exit") == 0)
+	else if (strcmp(args, "exit") == 0)
 		return Exit;
 	else
 		return Okay;
+}
+
+char** parseArguments(char* args) {
+	char** argsArray = malloc(MAX_ARG_LENGTH * sizeof(char*));
+	char* savePtr;
+	int counter = 0;
+
+	char* token = strtok_r(args, " ", &savePtr);
+	while (token != NULL) {
+		argsArray[counter] = token;
+		counter++;
+		token = strtok_r(NULL, " ", &savePtr);
+	}
+
+	argsArray[counter] = NULL; // Last array index will be NULL for iteration purposes
+	return argsArray;
 }
