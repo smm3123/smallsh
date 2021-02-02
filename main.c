711795e55385320044c9e5865fe10f1998db3bb1
smm3123 <syedmahdi3123@gmail.com>
2021-02-01 22:37:55 -0600

    Get user input

--- main.c
+++ main.c
@@ -7,28 +7,44 @@
 #define MAX_ARGS 512
 
 // Prototypes
+void getArguments(char* args);
 enum programState;
-enum programState handleInput();
+enum programState executeInput(char* args);
 
 enum programState {
 	Okay,
-	Failed
+	Exit
 };
 
 int main() {
 	enum programState state = Okay;
 	while (state == Okay) {
+		// Print and flush output buffer
 		printf(": ");
-		state = handleInput();
+		fflush(stdout);
+
+		// Get user input
+		char args[MAX_ARG_LENGTH];
+		getArguments(args);
+
+		state = executeInput(args);
 	}
 	return 0;
 }
 
-enum programState handleInput() {
-	int input;
-	scanf("%d", &input);
-	if (input == 1)
+void getArguments(char* args) {
+	// Get input and remove trailing new line
+	fgets(args, MAX_ARG_LENGTH, stdin);
+	args[strlen(args) - 1] = '\0';
+}
+
+enum programState executeInput(char* args) {
+	// Check if argument is null or comment
+	// If the first character in the arg is '#', it's considered a comment - midline comments not within scope of project
+	if ((args == NULL || strcmp(args, "") == 0) || args[0] == '#')
 		return Okay;
+	if (strcmp(args, "exit") == 0)
+		return Exit;
 	else
-		return Failed;
+		return Okay;
 }
