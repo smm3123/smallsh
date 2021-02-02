2a5ee25ae5b4b5c6b1ff2bc88c4133f1dcfab2ce
smm3123 <syedmahdi3123@gmail.com>
2021-02-02 13:34:55 -0600

    Create child processes for non-built-in commands

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h> 
+#include <sys/wait.h>
 
 // Constant global variables
 #define MAX_ARG_LENGTH 2048
@@ -13,6 +14,7 @@ void getArguments(char* userInput);
 char** parseArguments(char* args);
 enum programState executeInput(char** args);
 void executeCd(char** args);
+void executeNonBuiltInCommand(char** args);
 
 enum programState {
 	Okay,
@@ -31,6 +33,7 @@ int main() {
 		getArguments(userInput);
 		char** args = parseArguments(userInput);
 
+		// Execute the command and arguments
 		state = executeInput(args);
 	}
 	return 0;
@@ -43,6 +46,7 @@ void getArguments(char* userInput) {
 }
 
 char** parseArguments(char* args) {
+	// Stores each argument passed in by the user in an array index and returns the array
 	char** argsArray = malloc(MAX_ARG_LENGTH * sizeof(char*));
 	char* savePtr;
 	int counter = 0;
@@ -71,8 +75,10 @@ enum programState executeInput(char** args) {
 		executeCd(args);
 		return Okay;
 	}
-	else
+	else {
+		executeNonBuiltInCommand(args);
 		return Okay;
+	}
 }
 
 void executeCd(char** args) {
@@ -83,5 +89,28 @@ void executeCd(char** args) {
 	if (chdirStatus != 0) {
 		printf("Directory not found: %s\n", (args[1] == NULL) ? getenv("HOME") : args[1]);
 		fflush(stdout);
+	}
+}
+
+void executeNonBuiltInCommand(char** args) {
+	// Fork new process
+	int childStatus;
+	pid_t spawnPid = fork(); 
+	switch (spawnPid) {
+		case -1:
+			// Error creating new process
+			perror("fork()\n");
+			exit(1);
+			break;
+		case 0:
+			// In the child process
+			execvp(args[0], args);
+			perror("execv"); // exec will only return if there's an error
+			exit(2);
+			break;
+		default:
+			// In the parent process
+			spawnPid = waitpid(spawnPid, &childStatus, 0);
+			break;
 	}
 }
